3746e58d3ebe781ce52eccc5a92780536640c1a1
Oleksii Remizov <oleksii.remizov@synapse.com>
2017-10-12 16:02:45 +0300

    First commit

---
+++ MultithreadingWithInputFiles/GetAllFiles.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+
+namespace MultithreadingWithInputFiles
+{
+    public class GetAllFiles  
+    {
+        private Object thislock = new Object();
+        public List<string> FindAllFilesInDirectory(string directory)
+        {
+            string[] dirs;
+            List<string> dirsList = new List<string>();
+            dirs = Directory.GetFiles(directory, "*");
+            dirsList.AddRange(dirs);
+            return dirsList;
+        }
+
+        public string ReturnFileFromAll(List<string> dirs, int flag)
+        {
+            List<string> dirsList = dirs;
+            var flags = flag;
+            for(int i=0; i<flags; i++)
+                dirsList.Remove(dirsList.Last());
+            return dirsList.Last().ToString();
+        }
+        
+        public Dictionary<int, string> FindWordInTheFile(string linkOfFile, string curWord)
+        {
+            var wordsStat = new Dictionary<int, string>();
+            int amountInTheLine;
+            try
+            {
+                using (var streamReader = new StreamReader(linkOfFile))
+                {
+                    int numberOfString = 0;
+                    string inputLine;
+                    while (!streamReader.EndOfStream)
+                    {
+                        inputLine = streamReader.ReadLine();
+                        numberOfString++;
+                        amountInTheLine = new Regex(curWord).Matches(inputLine).Count;
+                        if (amountInTheLine > 0)
+                        {
+                            lock (thislock)
+                            {
+                                wordsStat[numberOfString] = linkOfFile;
+                            }
+                        }
+                    }
+                }
+                 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The programm failed with an error.");
+                Console.WriteLine(ex.ToString());
+            }
+            
+            return wordsStat;
+        }
+    }
+}
---
+++ MultithreadingWithInputFiles/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace MultithreadingWithInputFiles
+{
+    public class Program
+    {
+        public static string curWord = "VirtualCallManager";
+        public static int i = 0;
+        public static void Main(string[] args)
+        {
+            GetAllFiles getAllFiles = new GetAllFiles();
+            for (int i = 0; i < getAllFiles.FindAllFilesInDirectory(@"d:\logs!\").Count(); i++)
+            {
+                if (ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadProc), getAllFiles))
+                {
+
+                    Thread.Sleep(1000);
+                }
+            }
+            
+            Console.WriteLine("!!!Enter key!!!");
+            Console.ReadKey();
+        }
+
+        static void ThreadProc(object stateInfo)
+        {
+           
+            GetAllFiles getAllFiles = (GetAllFiles)stateInfo;
+            //var count = getAllFiles.FindAllFilesInDirectory(@"d:\logs!\").Count();//22
+            {
+                foreach (var wordsNumber in getAllFiles.FindWordInTheFile(getAllFiles.ReturnFileFromAll(getAllFiles.FindAllFilesInDirectory(@"d:\logs!\"), i), curWord).OrderByDescending(ws => ws.Value))
+                {
+                    Console.WriteLine("Substring {0} is in line #{1} and occurs <<< {2} >>> ---{3} ", "VirtualCallManager", wordsNumber.Key, wordsNumber.Value, Thread.CurrentThread.GetHashCode().ToString());
+
+                }
+                i++;
+            }
+            
+
+        }
+    }
+
+    
+}
